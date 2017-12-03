1076d53b7178cb6a3788e9f6795895139a520bd6
gofff <khatiullin@gmail.com>
2017-12-03 20:26:48 +0300

    add time

--- DPPE-R/src/main.cpp
+++ DPPE-R/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Functions.cpp"
 #include "DirichletProblem.h"
+#include <chrono>
 
 int main(int argc, char * argv[])
 {
@@ -12,8 +13,13 @@ int main(int argc, char * argv[])
 	const uint32_t n = atoi(argv[1]);
 	DirichletProblem<float> p(0.0, 0.0, 2.0, 2.0, n, n, FBoundaryFunc, FKnownFunc);
 	p.SetPrecision(1e-4);
+auto start = std::chrono::system_clock::now();
 	//float diff = p.Solve();
 	float diff = p.SolveMPI();
+auto end = std::chrono::system_clock::now();
+auto elapsed=std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+std::cout << "Time" << std::endl;
+std::cout << elapsed.count() << '\n';
 	//p.PrintSolution();
 	return 0;
 }
